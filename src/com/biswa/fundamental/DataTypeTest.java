4f0694d0df5782f94208a3c5d71204686e006b12
Biswaranjan <bmohapatra1983@gmail.com>
2025-09-25 22:35:58 +0530

    DataTypeTest Commit

---
+++ src/com/biswa/fundamental/DataTypeTest.java
@@ -0,0 +1,25 @@
+package com.biswa.fundamental;
+
+public class DataTypeTest {
+    public static void main(String[] args) {
+        byte byteVar = -128;//1 byte = 8 bit range -128 to 127
+        short shortVar = 10000;
+        int a = 10;
+        long longVar = 100000L;
+        float b = 20.5f;
+        double c = 30.99;
+        char d = 'A';
+        boolean e = false;
+        String f = "Hello, World!";
+        var abc = true;
+
+//        System.out.println("Integer: " + a);
+//        System.out.println("Float: " + b);
+//        System.out.println("Double: " + c);
+//        System.out.println("Character: " + d);
+//        System.out.println("Boolean: " + e);
+//        System.out.println("String: " + f);
+
+        System.out.println("Byte: " + byteVar);
+    }
+}
