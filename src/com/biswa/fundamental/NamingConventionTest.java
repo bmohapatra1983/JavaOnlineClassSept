727685edb22a94196771b505a9b385ccf7ad5c91
Biswaranjan <bmohapatra1983@gmail.com>
2025-09-25 22:19:32 +0530

    Naming Convention

---
+++ src/com/biswa/fundamental/NamingConventionTest.java
@@ -0,0 +1,16 @@
+package com.biswa.fundamental;
+
+public class NamingConventionTest {
+
+    void add123(){
+        float PI = 3.14F;
+    }
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int _a123;
+        int _123;
+        // int 1a; // Invalid variable name, commented out
+    }
+}
