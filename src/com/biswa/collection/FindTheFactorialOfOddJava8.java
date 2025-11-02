3221acbe76ffd19e6a676222c382f77f8c689f06
Biswaranjan <biswaranjan.mohapatra@swallowstack.com>
2025-11-02 22:37:22 +0530

    02/Nov

--- src/com/biswa/collection/FindTheFactorialOfOddJava8.java
+++ src/com/biswa/collection/FindTheFactorialOfOddJava8.java
@@ -2,15 +2,17 @@ package com.biswa.collection;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FindTheFactorialOfOddJava8 {
     public static void main(String[] args) {
         Integer[] arr = {1, 2, 3, 4, 5, 6, 7};
-        Arrays.stream(arr)
+        List<Integer> list = Arrays.stream(arr)
                 .filter(n -> n % 2 != 0)
                 .map(FindTheFactorialOfOddJava8::findFact)
-                .forEach(System.out::println);
+                .toList();
+        System.out.println(list);
     }
 
     static int findFact(int n) {
